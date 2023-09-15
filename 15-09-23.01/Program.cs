965fa6bf961403101b99c1c40090d5bfced4bd92
Miike <perezmiike2005@gmail.com>
2023-09-15 12:10:58 +0200

    NP

---
+++ 15-09-23.01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_09_23._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
