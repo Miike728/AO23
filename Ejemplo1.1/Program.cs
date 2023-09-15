88e6a3b5712f9d2cbb74f1fc8d3b619957233ace
Miike <perezmiike2005@gmail.com>
2023-09-15 11:28:57 +0200

    Upload

---
+++ Ejemplo1.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //abc
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.White;
+
+            //Cambiar titulo
+            Console.Title = "ABC";
+
+            Console.WriteLine("abc");
+
+            Console.Beep(20000, 2000);
+
+            /*a
+             * b
+             * c
+             */
+            Console.ReadLine();
+
+        }
+    }
+}
