5b5414a720ed954d55266bff0c9b7cfa0aad6187
Miike <perezmiike2005@gmail.com>
2023-09-20 12:39:12 +0200

    20/09

---
+++ 20-09-23.01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_09_23._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int base1;
+            int altura;
+
+            // Entrada base
+            Console.WriteLine("Base del triángulo: ");
+            base1 = int.Parse(Console.ReadLine());
+
+            // Entrada altura
+            Console.WriteLine("Altura del triángulo: ");
+            altura = int.Parse(Console.ReadLine());
+
+            // Calcular el área
+            int area = base1 * altura / 2;
+
+            // Imprimir el área
+            Console.WriteLine("El área del triángulo es de: " + area);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 20-09-23.03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_09_23._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("     *     *      ");
+            Console.WriteLine("    *   *   *       ");
+            Console.WriteLine("   *         *     ");
+            Console.WriteLine("  *           *   ");
+
+            Console.ReadLine();
+        }
+    }
+}
