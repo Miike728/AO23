8ec24ffdf4bea052a5493b0dc1e3c6970b55c13b
Miike <perezmiike2005@gmail.com>
2023-09-22 12:00:50 +0200

    Update Program.cs

--- 21-09-23.03/Program.cs
+++ 21-09-23.03/Program.cs
@@ -12,14 +12,18 @@ namespace _21_09_23._03
         {
             string dni;
             byte edad;
+            float salario;
 
             Console.Write("Introduce el DNI: ");
             dni = Console.ReadLine();
             Console.WriteLine();
 
-
             Console.Write("Introduce la edad: ");
             edad = byte.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Introduce el salario: ");
+            salario = float.Parse(Console.ReadLine());
             Console.WriteLine();
 
             Console.Write("DNI: " + dni);
