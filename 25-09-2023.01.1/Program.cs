8d41051a322e5ab7c0056c4a72979c413761bb5b
Miike <perezmiike2005@gmail.com>
2023-09-25 12:07:43 +0200

    Update Program.cs

--- 25-09-2023.01.1/Program.cs
+++ 25-09-2023.01.1/Program.cs
@@ -10,6 +10,19 @@ namespace _25_09_2023._01._1
     {
         static void Main(string[] args)
         {
+            byte n1 = 9;
+            short n1copia = n1;
+            int n1copia2 = n1copia;
+
+            float n2 = 4.5f;
+            double n2copia = n2;
+
+
+            int numero1 = 2400;
+            short numero1Copia = (short)numero1;
+            double numero2 = 5.678;
+            float numero2Copia = (float)numero2;
+
         }
     }
 }
