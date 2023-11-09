1e03ef90c8a9a14bf76e1e391246d028beb29078
Miike <perezmiike2005@gmail.com>
2023-11-09 13:29:22 +0100

    09/11

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 100;
+
+            while (i < 1500)
+            {
+                if (i % 2 == 0) 
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    i++;
+                    Console.Write(i + " ");
+                }
+                else
+                {                    
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    i++;
+                    Console.Write(i + " ");
+                }
+                System.Threading.Thread.Sleep(1);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RAM/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class ConsumoRAM
+{
+    static void Main(string[] args)
+    {
+        // Inicializar variables
+        int[] valoresAleatorios = new int[100000];
+        int milisegundos = 10;
+        int incremento = 1000000; // 10MB
+
+        // Bucle infinito
+        while (true)
+        {
+            // Generar valores aleatorios
+            for (int i = 0; i < valoresAleatorios.Length; i++)
+            {
+                valoresAleatorios[i] = new Random().Next();
+            }
+
+            // Mostrar el consumo de RAM
+            Console.WriteLine("Consumo de RAM: {0:F2} MB", valoresAleatorios.Length / 1048576);
+
+            // Aumentar el consumo de RAM
+            int[] nuevosValoresAleatorios = new int[valoresAleatorios.Length + incremento];
+            for (int i = 0; i < valoresAleatorios.Length; i++)
+            {
+                nuevosValoresAleatorios[i] = valoresAleatorios[i];
+            }
+            valoresAleatorios = nuevosValoresAleatorios;
+
+            // Esperar un milisegundo
+            System.Threading.Thread.Sleep(milisegundos);
+        }
+    }
+}
