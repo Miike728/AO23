8e1dc6c609717d8e3177389745bd168f344ec1d2
Miike <perezmiike2005@gmail.com>
2024-01-22 12:00:53 +0100

    22/01

---
+++ Arrays2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Introduce el numero de ejercicio: ");
+            string ejercicio = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("EJERCICIO " + ejercicio);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
