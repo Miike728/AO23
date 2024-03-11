0ee76ad83105db9146b23430a72c329463bf90f0
Miike <perezmiike2005@gmail.com>
2024-03-11 11:00:23 +0100

    11/03

--- colecciones2/Program.cs
+++ colecciones2/Program.cs
@@ -10,7 +10,86 @@ namespace colecciones2
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Colecciones 2");
+            Console.WriteLine();
 
+            // Crear una lista de tareas
+            List<string> tareasString = new List<string>();
+                        
+            // Introducir tarea desde consola
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Introduce una tarea (o 'fin' para terminar): ");
+            Console.ForegroundColor = ConsoleColor.White;
+            string tarea = Console.ReadLine();
+            while (tarea != "fin")
+            {
+                tareasString.Add(tarea);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("Introduce una tarea (o 'fin' para terminar): ");
+                Console.ForegroundColor = ConsoleColor.White;
+                tarea = Console.ReadLine();
+            }
+            
+            // Completar tarea: Elimina esa tarea y la pasa a una lista donde se almacenan las tareas completadas
+            List<string> tareasCompletadas = new List<string>();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Tareas pendientes:");
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int i = 0; i < tareasString.Count; i++)
+            {
+                Console.WriteLine(tareasString[i]);
+            }
+            
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Introduce el nombre de la tarea que quieres completar (o 'fin' para terminar): ");
+            Console.ForegroundColor = ConsoleColor.White;
+            string tareaCompletada = Console.ReadLine();
+            while (tareaCompletada != "fin")
+            {
+                tareasCompletadas.Add(tareaCompletada);
+                tareasString.Remove(tareaCompletada);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Tareas pendientes:");
+                Console.ForegroundColor = ConsoleColor.White;
+                for (int i = 0; i < tareasString.Count; i++)
+                {
+                    Console.WriteLine(tareasString[i]);
+                }
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("Introduce el nombre de la tarea que quieres completar (o 'fin' para terminar): ");
+                Console.ForegroundColor = ConsoleColor.White;
+                tareaCompletada = Console.ReadLine();
+            }
+
+
+            // Contar tareas pendintes
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Tareas pendientes: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(tareasString.Count);
+            Console.WriteLine();
+
+            // Mostrar tareas pendientes
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("Tareas pendientes:");
+            for (int i = 0; i < tareasString.Count; i++)
+            {
+                Console.WriteLine(tareasString[i]);
+            }
+            Console.WriteLine();
+
+            // Mostrar tareas completadas
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Tareas completadas:");
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int i = 0; i < tareasCompletadas.Count; i++)
+            {
+                Console.WriteLine(tareasCompletadas[i]);
+            }   
+
+            Console.ReadLine();
         }
     }
 }
