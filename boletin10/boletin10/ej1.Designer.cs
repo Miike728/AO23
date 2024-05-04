b0a49a8a282eb90c37cb1eaa8a08378a1307b0a7
Miike <perezmiike2005@gmail.com>
2024-05-04 18:42:36 +0200

    1

---
+++ boletin10/boletin10/ej1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace boletin10
+{
+    partial class ej1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkLaserJet = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chkBigNotebook = new System.Windows.Forms.CheckBox();
+            this.chkDigicam = new System.Windows.Forms.CheckBox();
+            this.chkMP4 = new System.Windows.Forms.CheckBox();
+            this.chkModem = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.chkSmartDesktop = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.numericLaserJet = new System.Windows.Forms.NumericUpDown();
+            this.numericBigNotebook = new System.Windows.Forms.NumericUpDown();
+            this.numericSmartDesktop = new System.Windows.Forms.NumericUpDown();
+            this.numericDigicam = new System.Windows.Forms.NumericUpDown();
+            this.numericMP4 = new System.Windows.Forms.NumericUpDown();
+            this.numericModem = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericLaserJet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericBigNotebook)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericSmartDesktop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericDigicam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericMP4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericModem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LaserJet X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Smart Desktop";
+            // 
+            // chkLaserJet
+            // 
+            this.chkLaserJet.AutoSize = true;
+            this.chkLaserJet.Location = new System.Drawing.Point(159, 60);
+            this.chkLaserJet.Name = "chkLaserJet";
+            this.chkLaserJet.Size = new System.Drawing.Size(50, 17);
+            this.chkLaserJet.TabIndex = 2;
+            this.chkLaserJet.Text = "$100";
+            this.chkLaserJet.UseVisualStyleBackColor = true;
+            this.chkLaserJet.CheckedChanged += new System.EventHandler(this.chkLaserJet_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Big Notebook";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(396, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "HD Digicam";
+            // 
+            // chkBigNotebook
+            // 
+            this.chkBigNotebook.AutoSize = true;
+            this.chkBigNotebook.Location = new System.Drawing.Point(171, 98);
+            this.chkBigNotebook.Name = "chkBigNotebook";
+            this.chkBigNotebook.Size = new System.Drawing.Size(50, 17);
+            this.chkBigNotebook.TabIndex = 5;
+            this.chkBigNotebook.Text = "$500";
+            this.chkBigNotebook.UseVisualStyleBackColor = true;
+            this.chkBigNotebook.CheckedChanged += new System.EventHandler(this.chkBigNotebook_CheckedChanged);
+            // 
+            // chkDigicam
+            // 
+            this.chkDigicam.AutoSize = true;
+            this.chkDigicam.Location = new System.Drawing.Point(511, 55);
+            this.chkDigicam.Name = "chkDigicam";
+            this.chkDigicam.Size = new System.Drawing.Size(44, 17);
+            this.chkDigicam.TabIndex = 6;
+            this.chkDigicam.Text = "$80";
+            this.chkDigicam.UseVisualStyleBackColor = true;
+            this.chkDigicam.CheckedChanged += new System.EventHandler(this.chkDigicam_CheckedChanged);
+            // 
+            // chkMP4
+            // 
+            this.chkMP4.AutoSize = true;
+            this.chkMP4.Location = new System.Drawing.Point(511, 93);
+            this.chkMP4.Name = "chkMP4";
+            this.chkMP4.Size = new System.Drawing.Size(50, 17);
+            this.chkMP4.TabIndex = 7;
+            this.chkMP4.Text = "$300";
+            this.chkMP4.UseVisualStyleBackColor = true;
+            this.chkMP4.CheckedChanged += new System.EventHandler(this.chkMP4_CheckedChanged);
+            // 
+            // chkModem
+            // 
+            this.chkModem.AutoSize = true;
+            this.chkModem.Location = new System.Drawing.Point(511, 136);
+            this.chkModem.Name = "chkModem";
+            this.chkModem.Size = new System.Drawing.Size(50, 17);
+            this.chkModem.TabIndex = 8;
+            this.chkModem.Text = "$150";
+            this.chkModem.UseVisualStyleBackColor = true;
+            this.chkModem.CheckedChanged += new System.EventHandler(this.chkModem_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(396, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "HiTech MP4";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(406, 140);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "ADSL Modem";
+            // 
+            // chkSmartDesktop
+            // 
+            this.chkSmartDesktop.AutoSize = true;
+            this.chkSmartDesktop.Location = new System.Drawing.Point(159, 136);
+            this.chkSmartDesktop.Name = "chkSmartDesktop";
+            this.chkSmartDesktop.Size = new System.Drawing.Size(50, 17);
+            this.chkSmartDesktop.TabIndex = 11;
+            this.chkSmartDesktop.Text = "$200";
+            this.chkSmartDesktop.UseVisualStyleBackColor = true;
+            this.chkSmartDesktop.CheckedChanged += new System.EventHandler(this.chkSmartDesktop_CheckedChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(76, 212);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "TOTAL =";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(149, 209);
+            this.txtTotal.Multiline = true;
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 42);
+            this.txtTotal.TabIndex = 13;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(385, 202);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 14;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(480, 202);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 15;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // numericLaserJet
+            // 
+            this.numericLaserJet.Location = new System.Drawing.Point(215, 57);
+            this.numericLaserJet.Name = "numericLaserJet";
+            this.numericLaserJet.Size = new System.Drawing.Size(76, 20);
+            this.numericLaserJet.TabIndex = 16;
+            this.numericLaserJet.ValueChanged += new System.EventHandler(this.numericLaserJet_ValueChanged);
+            // 
+            // numericBigNotebook
+            // 
+            this.numericBigNotebook.Location = new System.Drawing.Point(227, 97);
+            this.numericBigNotebook.Name = "numericBigNotebook";
+            this.numericBigNotebook.Size = new System.Drawing.Size(76, 20);
+            this.numericBigNotebook.TabIndex = 17;
+            // 
+            // numericSmartDesktop
+            // 
+            this.numericSmartDesktop.Location = new System.Drawing.Point(227, 135);
+            this.numericSmartDesktop.Name = "numericSmartDesktop";
+            this.numericSmartDesktop.Size = new System.Drawing.Size(76, 20);
+            this.numericSmartDesktop.TabIndex = 18;
+            // 
+            // numericDigicam
+            // 
+            this.numericDigicam.Location = new System.Drawing.Point(580, 52);
+            this.numericDigicam.Name = "numericDigicam";
+            this.numericDigicam.Size = new System.Drawing.Size(76, 20);
+            this.numericDigicam.TabIndex = 19;
+            // 
+            // numericMP4
+            // 
+            this.numericMP4.Location = new System.Drawing.Point(580, 90);
+            this.numericMP4.Name = "numericMP4";
+            this.numericMP4.Size = new System.Drawing.Size(76, 20);
+            this.numericMP4.TabIndex = 20;
+            // 
+            // numericModem
+            // 
+            this.numericModem.Location = new System.Drawing.Point(580, 136);
+            this.numericModem.Name = "numericModem";
+            this.numericModem.Size = new System.Drawing.Size(76, 20);
+            this.numericModem.TabIndex = 21;
+            // 
+            // ej1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericModem);
+            this.Controls.Add(this.numericMP4);
+            this.Controls.Add(this.numericDigicam);
+            this.Controls.Add(this.numericSmartDesktop);
+            this.Controls.Add(this.numericBigNotebook);
+            this.Controls.Add(this.numericLaserJet);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.chkSmartDesktop);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.chkModem);
+            this.Controls.Add(this.chkMP4);
+            this.Controls.Add(this.chkDigicam);
+            this.Controls.Add(this.chkBigNotebook);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chkLaserJet);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ej1";
+            this.Text = "ej1";
+            this.Load += new System.EventHandler(this.ej1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericLaserJet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericBigNotebook)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericSmartDesktop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericDigicam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericMP4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericModem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkLaserJet;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox chkBigNotebook;
+        private System.Windows.Forms.CheckBox chkDigicam;
+        private System.Windows.Forms.CheckBox chkMP4;
+        private System.Windows.Forms.CheckBox chkModem;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox chkSmartDesktop;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.NumericUpDown numericLaserJet;
+        private System.Windows.Forms.NumericUpDown numericBigNotebook;
+        private System.Windows.Forms.NumericUpDown numericSmartDesktop;
+        private System.Windows.Forms.NumericUpDown numericDigicam;
+        private System.Windows.Forms.NumericUpDown numericMP4;
+        private System.Windows.Forms.NumericUpDown numericModem;
+    }
+}
---
+++ boletin10/boletin10/ej1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace boletin10
+{
+    public partial class ej1 : Form
+    {
+        //Crear variable total
+        int total = 0;
+        //Varibles para los precios
+        int precioLaserJet = 100;
+        int precioBigNotebook = 500;
+        int precioSmartDesktop = 200;
+        int precioDigicam = 80;
+        int precioMP4 = 300;
+        int precioModem = 150;
+
+        //Variables para las cantidades
+        int cantLaserJet = 0;
+        int cantBigNotebook = 0;
+        int cantSmartDesktop = 0;
+        int cantDigicam = 0;
+        int cantMP4 = 0;
+        int cantModem = 0;
+
+        public ej1()
+        {
+            InitializeComponent();
+        }
+
+        private void ej1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void chkLaserJet_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkBigNotebook_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void chkSmartDesktop_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void chkDigicam_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void chkMP4_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void chkModem_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // Si chkLaserJet está seleccionado, sumar al total el precio de LaserJet * la cantidad
+            if (chkLaserJet.Checked)
+            {
+                cantLaserJet = (int)numericLaserJet.Value;
+                total += precioLaserJet * cantLaserJet;
+            }
+            if (chkBigNotebook.Checked)
+            {
+                cantBigNotebook = (int)numericBigNotebook.Value;
+                total += precioBigNotebook * cantBigNotebook;
+            }
+            if (chkSmartDesktop.Checked)
+            {
+                cantSmartDesktop = (int)numericSmartDesktop.Value;
+                total += precioSmartDesktop * cantSmartDesktop;
+            }
+            if (chkDigicam.Checked)
+            {
+                cantDigicam = (int)numericDigicam.Value;
+                total += precioDigicam * cantDigicam;
+            }
+            if (chkMP4.Checked)
+            {
+                cantMP4 = (int)numericMP4.Value;
+                total += precioMP4 * cantMP4;
+            }
+            if (chkModem.Checked)
+            {
+                cantModem = (int)numericModem.Value;
+                total += precioModem * cantModem;
+            }
+
+            //Mostrar el total en el textbox
+            txtTotal.Text = total.ToString();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            //Limpiar los checkbox y el textbox
+            chkLaserJet.Checked = false;
+            chkBigNotebook.Checked = false;
+            chkSmartDesktop.Checked = false;
+            chkDigicam.Checked = false;
+            chkMP4.Checked = false;
+            chkModem.Checked = false;
+            txtTotal.Text = "";
+            total = 0;
+        }
+
+        private void numericLaserJet_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
